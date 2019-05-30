e8c28e9df17f7320f01336d246a4deacbdbff7ac
bigmike3d <mbrown@alcaintl.com>
2019-05-29 20:36:36 -0400

    first commit

--- app/home/home-page.js
+++ app/home/home-page.js
@@ -3,12 +3,12 @@ const app = require("tns-core-modules/application");
 const HomeViewModel = require("./home-view-model");
 
 // Load config file which handles our session data
-var config = require("../shared/config");
+const config = require("../shared/config");
 
 function onNavigatingTo(args) {
     const page = args.object;
     const username = config.displayName;
-    console.log("COming from SESSION: " + config.displayName);
+    console.log(`Coming from SESSION: ${config.displayName}`);
     page.bindingContext = new HomeViewModel();
 }
 
