146e0813da8c68c4f5165e2bd21cb26695f37ef6
alcaitsolutions <45300987+alcaitsolutions@users.noreply.github.com>
2019-06-03 15:14:52 -0400

    bug fix

--- app/signup/signup-view-model.js
+++ app/signup/signup-view-model.js
@@ -1,28 +1,8 @@
 const observableModule = require("tns-core-modules/data/observable");
 
 function SignupViewModel() {
-    const viewModel = observableModule.fromObject({
-        fName: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-
-        signUp: function () {
-            const fName = this.fName,
-            alert("Validating Email!")
-            /* ***********************************************************
-            * Call your custom signup logic using the email and password data.
-            *************************************************************/
-        },
-
-        isValidEmail: function() {
-            var email = this.get("email");
-            alert("Validating Email!");
-        }
-
+   console.log("test");
       
-    });
-
     return viewModel;
 }
 
